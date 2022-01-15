aa6a1906dd97cfdf988e0b11c43811e815c2bf3d
honi20 <81567670+honi20@users.noreply.github.com>
2022-01-15 11:51:18 +0900

    Update 정영주.cpp

--- 홍익대 2021/23328/정영주.cpp
+++ 홍익대 2021/23328/정영주.cpp
@@ -1,1 +1,47 @@
-.
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, cnt = 0;
+char bomb, shield;
+string s, bombstr, shieldstr, a, b, d, e;
+
+int main() {
+    cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(NULL);
+
+    cin >> n >> bomb >> s;
+    shield = bomb - 'A' + 'a';
+
+    sort(s.begin(), s.end());
+
+    for (auto c : s) {
+        if (c == bomb) bombstr.push_back(c);
+        else if (c == shield) shieldstr.push_back(c);
+        else if (isalpha(c) == 1 && c < bomb) a.push_back(c);
+        else if (isalpha(c) == 1 && bomb < c) b.push_back(c);
+        else if (isalpha(c) == 2 && c < shield) d.push_back(c);
+        else e.push_back(c);
+    }
+
+    if (shieldstr.length() == 0) {
+        if (a.length() == 0) cout << bombstr + b + d + e << '\n';
+        else cout << a + b + d + e + bombstr << '\n';
+    }
+    else if (shieldstr.length() == 1) {
+        if (a.length() == 0) cout << bombstr + shieldstr + b + d + e << '\n';
+        else cout << a + b + d + e + shieldstr + bombstr << '\n';
+    }
+    else { // shieldstr.length() > 2
+        if (a.length() == 0) {
+            shieldstr.pop_back();
+            cout << bombstr + shield + b + d + shieldstr + e << '\n';
+        }
+        else {
+            shieldstr.pop_back();
+            cout << a + b + d + shield + bombstr + shieldstr + e << '\n';
+        }
+    }
+
+    return 0;
+}
