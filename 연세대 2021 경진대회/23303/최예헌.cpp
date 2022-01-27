40487dde644dddbae5cd79b097309efbb89d98c7
honi20 <81567670+honi20@users.noreply.github.com>
2022-01-28 00:53:04 +0900

    Update 최예헌.cpp

--- 연세대 2021 경진대회/23303/최예헌.cpp
+++ 연세대 2021 경진대회/23303/최예헌.cpp
@@ -1,1 +1,25 @@
-ㅇ
+//cin getline : 공백 포함 입력 문자열
+#include <iostream>
+using namespace std;
+
+int main(){
+    cin.tie(0); cout.tie(0);
+    ios::sync_with_stdio(false);
+    
+    string str;
+    bool flag = false;
+    
+    getline(cin,str);
+    for (int i=0;i<str.size();i++){
+        if (str[i]=='d' || str[i]=='D'){
+            if (str[i+1]=='2'){
+                flag=true;
+                break;
+            }
+        }
+    }
+    if (flag)
+        cout<<"D2";
+    else
+        cout<<"unrated";
+}
