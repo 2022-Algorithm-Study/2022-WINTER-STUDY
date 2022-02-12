5791e858f764ef80329c8ebd3eaa8f4142ebe489
honi20 <81567670+honi20@users.noreply.github.com>
2022-02-12 17:04:50 +0900

    Update 13235.cpp

--- 최예헌/2회차/13235.cpp
+++ 최예헌/2회차/13235.cpp
@@ -12,19 +12,11 @@ int main(){
     cin>>str;
     
     int size = str.size();
-    for (int i=0;i<(size/2-1);i++){
+    for (int i=0;i<=(size/2-1);i++){
         if (str[i] != str[size-1-i]){
             cout<<"false";
             return 0;
         }
     }
-    
-    if (size%2 == 0){
-        if (str[size/2] != str[size-1-(size/2)]){
-            cout<<"false";
-            return 0;
-        }
-    }
     cout<<"true";
-
 }
